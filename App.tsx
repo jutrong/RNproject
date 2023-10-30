44276e6c99308279f67af4be8f770058c95d9d4f
[SW5] 이주현 <leek71358@gmail.com>
2023-10-30 23:32:33 +0900

    login input

--- App.tsx
+++ App.tsx
@@ -1,75 +1,105 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Pressable } from "react-native";
+import { StyleSheet, Text, View, Pressable, TextInput } from "react-native";
 import { WebView } from "react-native-webview";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Icon from "react-native-vector-icons/MaterialIcons";
 
-const Tab = createBottomTabNavigator();
+function App() {
+  const [userInfo, setUserInfo] = useState({
+    email: "",
+    password: "",
+  });
 
-function HomeScreen() {
-  return <Text>Home</Text>;
-}
+  const onPressLogin = () => {
+    // Do something about login operation
+  };
+  const onPressForgotPassword = () => {
+    // Do something about forgot password operation
+  };
+  const onPressSignUp = () => {
+    // Do something about signup operation
+  };
 
-function SearchScreen() {
-  return <Text>Search</Text>;
-}
-
-function NotificationScreen() {
-  return <Text>Notification</Text>;
-}
-
-function MessageScreen() {
-  return <Text>Message</Text>;
-}
-
-function BottomTabNavigationApp() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home">
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            title: "홈",
-            tabBarIcon: ({ color, size }) => (
-              <Icon name="home" color={color} size={size} />
-            ),
-          }}
+    <View style={styles.container}>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Email"
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setUserInfo({ ...userInfo, email: text })}
         />
-        <Tab.Screen
-          name="Search"
-          component={SearchScreen}
-          options={{
-            title: "알림",
-            tabBarIcon: ({ color, size }) => (
-              <Icon name="notifications" color={color} size={size} />
-            ),
-          }}
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          secureTextEntry
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setUserInfo({ ...userInfo, password: text })}
         />
-        <Tab.Screen
-          name="Notification"
-          component={NotificationScreen}
-          options={{
-            title: "검색",
-            tabBarIcon: ({ color, size }) => (
-              <Icon name="search" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Message"
-          component={MessageScreen}
-          options={{
-            title: "메시지",
-            tabBarIcon: ({ color, size }) => (
-              <Icon name="message" color={color} size={size} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+      </View>
+      <Pressable onPress={onPressForgotPassword}>
+        <Text style={styles.forgot}>Forgot Password?</Text>
+      </Pressable>
+      <Pressable onPress={onPressLogin} style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN </Text>
+      </Pressable>
+      <Pressable onPress={onPressSignUp} style={styles.signupBtn}>
+        <Text>Signup</Text>
+      </Pressable>
+    </View>
   );
 }
 
-export default BottomTabNavigationApp;
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000000d8",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputView: {
+    width: "80%",
+    backgroundColor: "#3AB4BA",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20,
+  },
+  inputText: {
+    height: 50,
+    color: "white",
+  },
+  forgotAndSignUpText: {
+    color: "white",
+    fontSize: 11,
+  },
+  loginBtn: {
+    width: "80%",
+    backgroundColor: "#fb5b5a",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10,
+  },
+  loginText: {},
+  forgot: {},
+  signupBtn: {
+    width: "80%",
+    backgroundColor: "lightgray",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+});
