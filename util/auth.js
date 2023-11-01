34fcc027e90ca76818f206f9c9563d0d1fde8716
[SW5] 이주현 <leek71358@gmail.com>
2023-11-02 00:04:03 +0900

    .env APIkey 추가

--- util/auth.js
+++ util/auth.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const API_KEY = "AIzaSyBOr7MQDvnKSvvl7OUeaYkh6P4P5_WwdkE";
-
 export async function createUser(email, password) {
   const response = await axios.post(
     `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
