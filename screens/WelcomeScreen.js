02e86fc209a4c40c522769ac4cc6f8765e83a4c6
[SW5] 이주현 <leek71358@gmail.com>
2023-11-02 19:35:13 +0900

    firebase realtime database api

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,29 @@
 import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState("");
+  const authCtx = useContext(AuthContext);
+
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://gymduo-b54f8-default-rtdb.asia-southeast1.firebasedatabase.app/message.json?auth=${token}`
+      )
+      .then((response) => {
+        setFetchedMessage(response.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
